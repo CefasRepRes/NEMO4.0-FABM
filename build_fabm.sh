95db8866c3af1791f6b0f3aa3ca615b5bac871ed
John Aldridge (ENV) <john.aldridge@cefas.co.uk>
2024-01-29 13:14:56 +0000

    Undo previous change to gitignore and update batch files:

--- build_fabm.sh
+++ build_fabm.sh
@@ -8,11 +8,6 @@ module add netcdf/4.7.4/parallel/intel-2020-4-openmpi-cefas
 module add hdf5/1.10.6/intel-2020-4-openmpi-cefas
 module add mpi/openmpi/4.0.4/intel/ib-intel-2020-4-cefas
 
-#export FABM_HOME=/gpfs/software/ada/fabm/nemo/
-export FABM_HOME=/gpfs/home/$USER/local/fabm/nemo/
-export XIOS_HOME=/gpfs/software/ada/xios/2.5/intel-openmpi
-export NEMO_HOME=/gpfs/home/$USER/Projects/NEMO4.0-FABM
-
-ARCH="UEAADA_INTEL"
-CFG="AMM7_FABM_IOWt"
-REF="AMM7_FABM_IOW"
+cd ~/Projects/build_fabm/
+cmake ~/Projects/Dev_fabm/ -DFABM_HOST=nemo
+cmake --build . --target install
