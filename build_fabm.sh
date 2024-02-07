f3ef4415acaf83029972a4f5ef05763a4a69b07e
John Aldridge (ENV) <john.aldridge@cefas.co.uk>
2024-02-07 18:20:09 +0000

    Added script to build fabm from the NEMO dir

--- build_fabm.sh
+++ build_fabm.sh
@@ -8,6 +8,11 @@ module add netcdf/4.7.4/parallel/intel-2020-4-openmpi-cefas
 module add hdf5/1.10.6/intel-2020-4-openmpi-cefas
 module add mpi/openmpi/4.0.4/intel/ib-intel-2020-4-cefas
 
-cd ~/Projects/build_fabm/
-cmake ~/Projects/Dev_fabm/ -DFABM_HOST=nemo
+fabmdir=fabm_dev.git
+cd ~/Projects/$fabmdir/build/
+cmake ../ -DFABM_HOST=nemo
 cmake --build . --target install
+
+#cd ~/Projects/Dev_fabm/build_fabm/
+#cmake ~/Projects/Dev_fabm/ -DFABM_HOST=nemo
+#cmake --build . --target install
