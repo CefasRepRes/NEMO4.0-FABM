f6c60dbdcbcbe2d07de3e4df473324cd68caed98
Robert Mcewan (ENV) <dzd22eyu@c0009.hpc.private>
2025-02-14 12:54:38 +0000

    adding in add_mod_intel_ersem.sh for ersem builds

---
+++ add_mod_ada_intel_ersem.sh
@@ -0,0 +1,24 @@
+source /etc/profile.d/modules.sh
+module purge
+
+module load git/2.27.0
+module load cmake/3.16.5
+module add intel/2020-4
+module add netcdf/4.7.4/parallel/intel-2020-4-openmpi-cefas
+module add hdf5/1.10.6/intel-2020-4-openmpi-cefas
+module add mpi/openmpi/4.0.4/intel/ib-intel-2020-4-cefas
+
+#export FABM_HOME=/gpfs/home/uck09rvu/git/fabm
+#export FABM_HOME=/gpfs/software/ada/fabm/nemo/
+export FABM_HOME=/gpfs/home/$USER/local/fabm/nemo-ersem/
+export XIOS_HOME=/gpfs/software/ada/xios/2.5/intel-openmpi
+export NEMO_HOME=/gpfs/home/$USER/NEMO4.0-FABM
+export ERSEM_HOME=/gpfs/data/cefas-shared/NEMO-benchmark-cfg/code/ersem
+
+#ARCH="UEAADA_INTEL"
+#CFG="AMM12-test"
+#REF="AMM12"
+
+ARCH="UEAADA_INTEL"
+CFG="AMM7_FABM_ERSEM_MO"
+REF="AMM7_FABM"
