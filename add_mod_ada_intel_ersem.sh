e22b2cc7e8f1a3c94079a617661be429d4de8933
Robert Mcewan (ENV) <dzd22eyu@c0002.hpc.private>
2025-03-07 13:26:27 +0000

    adding cfgs/AMM7_FABM/MY_SRC and updating make scripts

--- add_mod_ada_intel_ersem.sh
+++ add_mod_ada_intel_ersem.sh
@@ -12,7 +12,7 @@ module add mpi/openmpi/4.0.4/intel/ib-intel-2020-4-cefas
 #export FABM_HOME=/gpfs/software/ada/fabm/nemo/
 export FABM_HOME=/gpfs/home/$USER/local/fabm/nemo-ersem/
 export XIOS_HOME=/gpfs/software/ada/xios/2.5/intel-openmpi
-export NEMO_HOME=/gpfs/home/$USER/NEMO4.0-FABM
+export NEMO_HOME=/gpfs/home/$USER/TEST_ERSEM/NEMO4.0-FABM
 export ERSEM_HOME=/gpfs/data/cefas-shared/NEMO-benchmark-cfg/code/ersem
 
 #ARCH="UEAADA_INTEL"
