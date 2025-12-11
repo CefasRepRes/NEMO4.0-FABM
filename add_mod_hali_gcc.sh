d36d93bddf05b2c8644ed392749ec77d70eb96cc
tiagoams <tiago.silva@cefas.co.uk>
2025-12-11 20:10:43 +0000

    add support gcc 9
    Tested with NEMO-ERSEM

--- add_mod_hali_gcc.sh
+++ add_mod_hali_gcc.sh
@@ -8,8 +8,7 @@ module add mpi/openmpi/4.0.3/gcc/ib
 module add netcdf/4.7.4/parallel/gcc-openmpi
 module add hdf5/1.10.6/gcc-openmpi
 
-#export FABM_HOME=/gpfs/home/uck09rvu/git/fabm
-export FABM_HOME=/gpfs/home/uck09rvu/local/fabm/nemo/
+export FABM_HOME=/gpfs/home/uck09rvu/local/fabm/nemo-ersem-hali-gcc
 export XIOS_HOME=/gpfs/home/uck09rvu/git/xios-2.5
 export NEMO_HOME=/gpfs/home/uck09rvu/git/NEMO4.0-FABM
 
--- add_mod_hali_intel.sh
+++ add_mod_hali_intel.sh
@@ -1,19 +1,18 @@
 source /etc/profile.d/modules.sh
 module purge
 
+module add cmake/3.31.8
+module add intel/2025.3.0
+module add netcdf/4.7.4/parallel/intel-2025-openmpi-cefas-nemo
+module add hdf5/1.14.6/intel-2025-openmpi-cefas-nemo
+module add mpi/openmpi/4.1.6/intel/2025-ib-cefas-nemo
 
-
-module add cmake/3.16.0-rc4
-module add intel/2025.1.0
-module add netcdf/4.7.4/parallel/intel-2025-openmpi-cefas
-module add hdf5/1.10.6/intel-2025-openmpi-cefas
-module add mpi/openmpi/4.1.6/intel/2025-ib
-
-#export FABM_HOME=/gpfs/home/uck09rvu/git/fabm
-export FABM_HOME=/gpfs/home/uck09rvu/local/fabm/nemo/
-export XIOS_HOME=/gpfs/home/uck09rvu/git/xios-2.5
-export NEMO_HOME=/gpfs/home/uck09rvu/git/NEMO4.0-FABM
+#export FABM_HOME=/gpfs/home/$USER/local/fabm/nemo-ersem-hali/
+export FABM_HOME=/gpfs/home/$USER/local/fabm/nemo-ersem-hali/
+export XIOS_HOME=/gpfs/home/dzd22eyu/xios-2.5-cefas
+export NEMO_HOME=/gpfs/home/$USER/git/NEMO4.0-FABM
+export ERSEM_HOME=/gpfs/data/cefas-shared/NEMO-benchmark-cfg/code/ersem
 
 ARCH="UEAHALI_INTEL"
-CFG="newconfig2"
+CFG="AMM7_FABM_ERSEM_HALI"
 REF="AMM7_FABM"
